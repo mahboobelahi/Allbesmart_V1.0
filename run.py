2ac835f654acd433f8e471b7aacd601b4866171c
Mahboob Elahi (TAU) <mahboob.elahi@tuni.fi>
2022-06-24 11:36:44 +0300

    no update

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import threading
+import threading,time
 from FASToryEvents_EM import app
 from FASToryEvents_EM import UtilityFunctions as helper
 from FASToryEvents_EM import configurations as CONFIG
@@ -7,24 +7,20 @@ from FASToryEvents_EM.dbModels import EnergyMeasurements, WorkstationInfo,Measur
 
 
 if __name__ == '__main__':
-    # query = MeasurementsForDemo.query.all()
-    # fileName='forDemo.csv'
-    # helper.SQL_queryToCsv(fileName,query)
-    # mqtt = MqqtClient(CONFIG.NAME,CONFIG.MQTT_CLIENT_ID)
-    # mqtt.connect()
-    
-    #helper.createModels()    
+   
     # Workstation Objects
-    # start_workstations=threading.Thread(target=helper.Workstations)
-    # start_workstations.daemon=True
-    # start_workstations.start()
-    #event subscriptions for orchestrator
-    # orc_subscriptions=threading.Thread(target=helper.EventSubscriptions)
-    # orc_subscriptions.daemon=True
-    # orc_subscriptions.start()
-    #time.sleep(5)
-    #helper.get_local_ip()
-    helper.createModels()
+    start_workstations=threading.Thread(target=helper.Workstations)
+    start_workstations.daemon=True
+    start_workstations.start()
+
+    # #event subscriptions for orchestrator
+
+    # time.sleep(6)
+    orc_subscriptions=threading.Timer(3,helper.EventSubscriptions,args=(WorkstationInfo.query.all(),))
+    orc_subscriptions.daemon=True
+    orc_subscriptions.start()
+    # #helper.get_local_ip()
+    #helper.createModels()
     app.run(host='0.0.0.0', port=CONFIG.appLocPort,debug=False)#,use_reloader=False,debug=True
 
 

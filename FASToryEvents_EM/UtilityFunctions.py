ee6639990be235dd08420cd80eea0a7df90070d9
Mahboob Elahi (TAU) <mahboob.elahi@tuni.fi>
2022-06-24 11:41:06 +0300

    MQTT message parsing funtion was updated

--- FASToryEvents_EM/UtilityFunctions.py
+++ FASToryEvents_EM/UtilityFunctions.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 import threading,time,requests,json,socket,csv
 from FASToryEvents_EM import FASToryWorkstations as WkS
 from FASToryEvents_EM.configurations import *
@@ -5,6 +6,7 @@ from FASToryEvents_EM.dbModels import EnergyMeasurements, WorkstationInfo,Measur
 from FASToryEvents_EM import db
 from netifaces import interfaces, ifaddresses, AF_INET
 from sqlalchemy.ext.serializer import loads, dumps
+from sqlalchemy.exc import SQLAlchemyError
 
 #under test
 def reslove_update_IP():
@@ -27,15 +29,20 @@ def createModels():
     db.create_all()
     db.session.commit()
 
+
+
 #subscription for Orchestrator
 def EventSubscriptions(result):
     body = {"destUrl": f'http://{wrkCellLocIP}:{appLocPort}/api/logCellEvents'}                               
     for workCell in result:
         #robot event subscription if possible 
+        if workCell.id !=2 and workCell.id !=10 :# and id!=1:
+            continue
         if workCell.ComponentStatus[0]:
+            print(f'[XU-R_Subscriptions]')
             for eventID in RobotEvents:
                 try:
-                    print(f'[X-U]:Robot_{workCell.id}')
+                    
                     ROB_RTU_Url_s = f'http://192.168.{str(workCell.id)}.1/rest/events/{eventID}/notifs'
                     
                     r = requests.post(ROB_RTU_Url_s, json=body)
@@ -45,6 +52,7 @@ def EventSubscriptions(result):
 
         #conveyor zone event subscription if possible
         if workCell.ComponentStatus[1]:
+            print(f'[XU-C_Subscriptions]')
             if workCell.HasZone4:
                 for eventID in ConveyorEvents:    
                     try:
@@ -65,12 +73,21 @@ def EventSubscriptions(result):
                     except requests.exceptions.RequestException as err:
                         print("[X-E] OOps: Something Else", err) 
 
+            if workCell.HasEM_Module:
+                try:
+                    E10_Url = f'http://192.168.{str(workCell.id)}.4/rest/events/send_rest/notifs' 
+                    r = requests.post(E10_Url, json= {"destUrl": "http://192.168.189.101:2000/api/powerEvents"})
+                    print(f'[XFW]:WorkCell_{workCell.id}has subscribed: E10 event with request code: {r.status_code}.')
+                except requests.exceptions.RequestException as err:
+                    print("[X-E] OOps: Something Else", err) 
+
 #Unsubscription for Orchestrator
 def EventUnSubscriptions(result):
 
     for workCell in result:
         #robot event Unsubscription if possible 
         if workCell.ComponentStatus[0]:
+            print(f'[XU-R_UnSubscriptions]')
             for eventID in RobotEvents:
                 try:
                     ROB_RTU_Url_s = f'http://192.168.{str(workCell.id)}.1/rest/events/{eventID}/notifs'
@@ -81,6 +98,7 @@ def EventUnSubscriptions(result):
 
         #conveyor zone event Unsubscription if possible
         if workCell.ComponentStatus[1]:
+            print(f'[XU-C_UnSubscriptions]')
             if workCell.HasZone4:
                 for eventID in ConveyorEvents:    
                     try:
@@ -99,6 +117,14 @@ def EventUnSubscriptions(result):
                         print(f'[X-U]:CNV_{workCell.id} has Unsubscribed:{eventID} event with request code: {r.status_code}.')
                     except requests.exceptions.RequestException as err:
                         print("[X-E] OOps: Something Else", err) 
+            
+            if workCell.HasEM_Module:
+                try:
+                    E10_Url = f'http://192.168.{str(workCell.id)}.4/rest/events/send_rest/notifs' 
+                    r = requests.delete(E10_Url)
+                    #print(f'[XFW]:WorkCell_{workCell.id}has subscribed:{eventID} event with request code: {r.status_code}.')
+                except requests.exceptions.RequestException as err:
+                    print("[X-E] OOps: Something Else", err) 
 
 #accessing JWT token
 def get_access_token():
@@ -188,6 +214,76 @@ def dump_datetime(value):
             return None
         return [value.strftime("%Y-%m-%d"), value.strftime("%H:%M:%S")]
 
+def parseCommand(message):
+    try:
+        body={"destUrl" : ""}
+        payload=json.loads(message.payload)
+        if payload.get('data'):
+            payload= payload.get('data')
+        
+        #db will handles
+        exID = int(payload.get("external_ID").split('4')[0])
+        result = WorkstationInfo.query.get(exID)
+        if payload.get("send_power_measurements") and result.HasEM_Module:
+            cmd = payload.get("send_power_measurements")
+            if cmd =="start":
+                # check previous EM subscriptions
+                req = requests.get(f'http://192.168.{result.id}.4/rest/events/send_rest/notifs')
+                print(req.json().get("children"))
+                if req.json().get("children"):
+                    req = requests.post(result.EM_service_url,json={"destUrl": "http://192.168.100.100:2002/api/measurements"})
+                    print(f'[X] {str(req.status_code)+req.reason}')
+                
+                else:
+                    req = requests.post(f'http://192.168.{result.id}.4/rest/events/send_rest/notifs',
+                                        json={"destUrl": "http://192.168.100.100:2002/api/measurements"})
+                    req = requests.post(result.EM_service_url,json=body)
+                    print(f'[X] {str(req.status_code)+req.reason}')
+            else:
+                E10_Url = f'http://192.168.{str(result.id)}.4/rest/events/send_rest/notifs'
+                req = requests.delete(E10_Url)
+                print(f'[X] {str(req.status_code)+req.reason}')
+        
+        if  result.ComponentStatus[0]:#(payload.get("change_pen") or payload.get("draw_component")) and
+            
+            if payload.get("calibrate_Robot"):
+                req = requests.post(f'{result.Robot_service_url+payload.get("calibrate_Robot")}',json=body)
+                print(f'[X] service_{payload.get("calibrate_Robot")+str(req.status_code)+req.reason}')
+            
+            if payload.get("change_pen"):
+                #print(f'[X] {result.Robot_service_url+payload.get("change_pen")}')
+                req = requests.post(f'{result.Robot_service_url+payload.get("change_pen")}',json=body)
+                print(f'[X] service_{payload.get("change_pen")+str(req.status_code)+req.reason}')
+            
+            if payload.get("draw_component"):
+                #print(f'[X] {result.Robot_service_url+payload.get("draw_component")}')
+                req = requests.post(f'{result.Robot_service_url+payload.get("draw_component")}',json=body)
+                print(f'[X] service_{payload.get("draw_component")+str(req.status_code)+req.reason}')
+
+        if payload.get("move_pallet"):
+            
+            destinamtion = payload.get("move_pallet")
+            if ((destinamtion == 'TransZone14' or destinamtion == 'TransZone45')and result.HasZone4 == False):
+                req = requests.post(f'{result.CNV_service_url+payload.get("move_pallet")}',json=body)
+                print(f'[X] {str(req.status_code)+req.reason}')
+            else:
+                req = requests.post(f'{result.CNV_service_url+payload.get("move_pallet")}',json=body)
+                print(f'[X] {str(req.status_code)+req.reason}')
+                
+    except ValueError:
+        print('[X-Routes] Decoding JSON has failed')
+    except SQLAlchemyError as e:
+        print(f'[X]',str(e.__dict__['orig']))
+    except requests.exceptions.RequestException as err:
+        print("[X] OOps:HTTP-Error", err)
+
+# {
+#     "external_ID":"104EM",
+#     "send_power_measurements": "start",
+#     "move_pallet":"TransZone12",
+#     "change_pen" :"ChangePenRED",
+#     "draw_component":"Draw1"
+# }
 #sending events from Simulatorfile
 def simulateData(externalId,measurements,payload,
                         access_token_time,expire_time,headers):     
@@ -206,10 +302,10 @@ def simulateData(externalId,measurements,payload,
                                             headers=headers)
                     req_event = requests.post(  url=f'{SYNCH_URL}/sendCustomMeasurement',
                                                 params=payload,headers=headers,
-                                                json={"SimEvent": event[i].get("event")})
+                                                json={"value": event[i].get("event")})
                     #checking for token lifetime
                     print(f'[X-RS] ({req_A.status_code},{req_V.status_code},{req_P.status_code},{req_event.status_code},{i})')
-
+                    print(event[i].get("event"))
                     time.sleep(1)
         except requests.exceptions.RequestException as err:
                 print("[X-E] OOps: Something Else", err)
@@ -224,32 +320,31 @@ def simulateData(externalId,measurements,payload,
 #workcell obj
 def Workstations():
     
-    for id in range(1,12):
-        # if id !=10 :# and id!=1:
-        #     continue
+    for id in range(1,13):
+        if id !=2 :#and id!=10 :# and id!=1:
+            continue
         temp_obj = WkS.Workstation(id,wrkCellLocIP,
                                     make[id-1],type[id-1],
                                     wrkCellLocPort+id,numFast=3,num=3)
-        temp_obj.WkSINFO()
+        #temp_obj.WkSINFO()
+
         #subscribing to conveyor Zone events
-        #temp_obj.LineEventsSubscription()
+        temp_obj.LineEventsSubscription()
         #temp_obj.UnSubscribeToLineEvents()
         
         #deleting past subscription to EM service.
         #temp_obj.invoke_EM_service()
         #now invoke EM service for accurate results
-        #temp_obj.get_access_token()
         # send_measurements=threading.Timer(8,temp_obj.invoke_EM_service,args=("start",))
         # send_measurements.daemon=True
         # send_measurements.start()
         #startring server for workstation
         threading.Thread(target=temp_obj.runApp,daemon=True).start()
-        
+        temp_obj.get_access_token()
         #wait a while for server initialization
-        time.sleep(1)
+        #time.sleep(1)
         #check device registration or register device to ZDMP-DAQ component 
         #temp_obj.register_device()
-
         #subscribe device for ASYNC data access
         #temp_obj.sub_or_Unsubscribe_DataSource(True)
 
